1024c4adeea1250e298cde80f7e360711ff27448
José Luis Millán <jmillan@aliax.net>
2020-11-09 18:07:40 +0100

    emitter: listen events on custom document

--- core/emitter.js
+++ core/emitter.js
@@ -1,25 +1,36 @@
 import EventEmitter from 'eventemitter3';
 import logger from './logger';
+import Quill from './quill';
 
 let debug = logger('quill:events');
 
 const EVENTS = ['selectionchange', 'mousedown', 'mouseup', 'click'];
 
-EVENTS.forEach(function(eventName) {
-  document.addEventListener(eventName, (...args) => {
-    [].slice.call(document.querySelectorAll('.ql-container')).forEach((node) => {
-      // TODO use WeakMap
-      if (node.__quill && node.__quill.emitter) {
-        node.__quill.emitter.handleDOM(...args);
-      }
-    });
-  });
-});
-
+let initialized = false;
 
 class Emitter extends EventEmitter {
+  static initialize() {
+    if (initialized) return;
+
+    EVENTS.forEach(function(eventName) {
+      Quill.getDocument().addEventListener(eventName, (...args) => {
+        [].slice.call(Quill.getDocument().querySelectorAll('.ql-container')).forEach((node) => {
+          // TODO use WeakMap
+          if (node.__quill && node.__quill.emitter) {
+            node.__quill.emitter.handleDOM(...args);
+          }
+        });
+      });
+    });
+
+    initialized = true;
+  }
+
+
   constructor() {
     super();
+
+    Emitter.initialize();
     this.listeners = {};
     this.on('error', debug.error);
   }
